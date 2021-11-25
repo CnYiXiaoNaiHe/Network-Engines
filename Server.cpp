5045a96e07cc9b8e8078c59229025cb97b9ad09e
CnYiXiaoNaiHe <1969118063@qq.com>
2021-11-25 20:06:56 +0800

    0.0.1
    Windows版本Server服务

---
+++ Server.cpp
@@ -0,0 +1,71 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include<Winsock2.h>
+#include<windows.h>
+#include<cstdio>
+
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+    WORD ver = MAKEWORD(2, 2);
+    WSADATA dat;
+    WSAStartup(ver,&dat);
+
+    SOCKET sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    
+    sockaddr_in _sin = {};
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = (htons)4567;
+    _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //INADDR_ANY
+
+
+    if(SOCKET_ERROR ==bind(_sock, (sockaddr*)&_sin,sizeof(_sin)))
+    {
+         printf("bind sockaddr_in error!\n");
+    }
+    else
+    {
+        printf("bind sockaddr_in ok!\n");
+    }
+
+    if(SOCKET_ERROR == listen(_sock, 5))
+    {
+        printf("listen error!\n");
+    }
+        else
+    {
+        printf("listen ok!\n");
+    }
+    
+    sockaddr_in clientAddr = {};
+    int nAddrLen = sizeof(sockaddr_in);
+    SOCKET _cSock = INVALID_SOCKET;
+    char msgBuf[] = "hello , This is Server!";
+
+    while (true)
+    {
+     _cSock = accept(_sock,(sockaddr*)&clientAddr,&nAddrLen);
+    if(INVALID_SOCKET == _cSock)
+    {
+        printf("accept error!\n");
+    }
+
+    printf("New connections join,Ip: %d",inet_ntoa(clientAddr.sin_addr));
+
+    send(_cSock,msgBuf,strlen(msgBuf) + 1,0); 
+
+   }
+   
+
+  
+
+
+    closesocket(_sock);
+
+
+    WSACleanup();
+    return 0;
+}
